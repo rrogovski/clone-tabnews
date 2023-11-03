0f3f0581a65bbc5cf01015ffcc31308cc5a2f61c
Rodrigo Rogovski <rfrogovski@gmail.com>
2023-11-03 15:02:51 -0400

    Teste commit

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ function Home() {
   return (
     <h1>
       Diante da vastidão do tempo e da imensidão do universo, é um imenso prazer
-      para mim dividir um planeta e uma época com você.
+      para mim dividir um planeta e uma época com você. Carl Sagan!
     </h1>
   );
 }
