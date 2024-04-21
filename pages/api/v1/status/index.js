b4c6892f1fa912a0232e60d2d1f0b17f79e19578
Rodrigo Rogovski <rfrogovski@gmail.com>
2024-04-21 15:17:17 -0400

    add `database.js` and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,9 @@
-function status(req, res) {
+import database from '../../../../infra/database.js';
+
+async function status(req, res) {
+  const result = await database.query('SELECT NOW()');
+  console.log(result);
+  console.log(process.env);
   res.status(200).json({ status: 'ok' });
 }
 
