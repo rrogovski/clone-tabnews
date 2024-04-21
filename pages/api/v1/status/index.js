29da6f5230a700c5b4171a54c80dc54e19908618
Rodrigo Rogovski <rfrogovski@gmail.com>
2024-04-21 15:44:26 -0400

    move `.env` file to `.env.development`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -3,7 +3,6 @@ import database from '../../../../infra/database.js';
 async function status(req, res) {
   const result = await database.query('SELECT NOW()');
   console.log(result);
-  console.log(process.env);
   res.status(200).json({ status: 'ok' });
 }
 
