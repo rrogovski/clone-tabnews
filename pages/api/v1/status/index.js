cb24fccd73952d531ba698f0a66a794606e88b45
Rodrigo Rogovski <rfrogovski@gmail.com>
2024-04-22 22:51:59 -0400

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,31 @@
 import database from '/infra/database.js';
 
 async function status(req, res) {
-  const result = await database.query('SELECT NOW()');
-  console.log(result);
-  res.status(200).json({ status: 'ok' });
+  const result = await database.query({
+    text: `SELECT 
+      NOW() as now,
+      VERSION() as full_version,
+      current_setting('server_version') as version,
+      current_setting('max_connections')::int as max_connections,
+      count(*)::int as opened_connections
+      FROM pg_stat_activity
+      where datname = $1;`,
+    values: [process.env.POSTGRES_DB],
+  });
+
+  const response = {
+    now: result.rows[0].now,
+    dependencies: {
+      database: {
+        full_version: result.rows[0].full_version,
+        version: result.rows[0].version,
+        max_connections: result.rows[0].max_connections,
+        opened_connections: result.rows[0].opened_connections,
+      },
+    },
+  };
+
+  return res.status(200).json(response);
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,19 +1,27 @@
 test('GET to /api/v1/status should return 200', async () => {
   const response = await fetch(`http://localhost:3000/api/v1/status`);
   expect(response.status).toBe(200);
-});
 
-test('GET to /api/v1/status should return 200 by callback', () => {
-  return fetch(`http://localhost:3000/api/v1/status`).then((response) => {
-    expect(response.status).toBe(200);
-  });
-});
+  const responseBody = await response.json();
 
-test('GET to /api/v1/status should return 200 with Promise', () => {
-  return new Promise((resolve) => {
-    fetch(`http://localhost:3000/api/v1/status`).then((response) => {
-      expect(response.status).toBe(200);
-      resolve();
-    });
-  });
+  expect(responseBody).toHaveProperty('now');
+
+  const parsedDate = new Date(responseBody.now).toISOString();
+  expect(parsedDate).toMatch(/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z$/);
+  expect(parsedDate).toEqual(responseBody.now);
+
+  // expect(responseBody).toHaveProperty('version');
+  expect(responseBody.dependencies.database.full_version).toMatch(
+    /^\w+ \d+\.\d+/,
+  );
+
+  expect(responseBody.dependencies.database.version).toMatch(/^\d+\.\d+/);
+
+  // expect(responseBody).toHaveProperty('max_connections');
+  // expect(responseBody.dependencies.database.max_connections).toMatch(/^\d+$/);
+  expect(responseBody.dependencies.database.max_connections).toBeGreaterThan(0);
+
+  // expect(responseBody).toHaveProperty('current_connections');
+  // expect(responseBody.dependencies.database.current_connections).toMatch(/^\d+$/,);
+  expect(responseBody.dependencies.database.opened_connections).toEqual(1);
 });
