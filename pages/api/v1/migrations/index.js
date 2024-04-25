f132b25b2dbe39cffd5dea10dc0007927264d793
Rodrigo Rogovski <rfrogovski@gmail.com>
2024-04-25 01:29:21 -0400

    Fix `/api/v1/migrations` endpoint

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -4,35 +4,40 @@ import database from 'infra/database';
 
 export default async function migrations(req, res) {
   if (!['GET', 'POST'].includes(req.method)) {
-    return res.status(405).end();
+    return res.status(405).json({ error: `Method ${req.method} Not Allowed` });
   }
 
-  const dbClient = await database.getNewClient();
-  const defaultMigrationsOptions = {
-    dbClient,
-    dryRun: true,
-    dir: join('infra', 'migrations'),
-    direction: 'up',
-    verbose: true,
-    migrationsTable: 'pgmigrations',
-  };
+  let dbClient;
 
-  if (req.method === 'GET') {
-    const pendingMigrations = await migrationRunner(defaultMigrationsOptions);
+  try {
+    dbClient = await database.getNewClient();
+    const defaultMigrationsOptions = {
+      dbClient,
+      dryRun: true,
+      dir: join('infra', 'migrations'),
+      direction: 'up',
+      verbose: true,
+      migrationsTable: 'pgmigrations',
+    };
+
+    if (req.method === 'GET') {
+      const pendingMigrations = await migrationRunner(defaultMigrationsOptions);
+      return res.status(200).json(pendingMigrations);
+    }
+
+    if (req.method === 'POST') {
+      const migratedMigrations = await migrationRunner({
+        ...defaultMigrationsOptions,
+        dryRun: false,
+      });
+      return res
+        .status(migratedMigrations.length > 0 ? 201 : 200)
+        .json(migratedMigrations);
+    }
+  } catch (error) {
+    console.error(error);
+    throw error;
+  } finally {
     await dbClient.end();
-    return res.status(200).json(pendingMigrations);
   }
-
-  if (req.method === 'POST') {
-    const migratedMigrations = await migrationRunner({
-      ...defaultMigrationsOptions,
-      dryRun: false,
-    });
-    await dbClient.end();
-    return res
-      .status(migratedMigrations.length > 0 ? 201 : 200)
-      .json(migratedMigrations);
-  }
-
-  return res.status(405).end();
 }
