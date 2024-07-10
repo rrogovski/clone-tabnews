78ae3cb5af5213ceb6504b8cd5c8766b34e0b2fd
Rodrigo Rogovski <rfrogovski@gmail.com>
2024-07-09 22:42:05 -0400

    makes `npm test` more robust with `async-retry` and `orchestrator.js`

--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -6,9 +6,12 @@ async function waitForAllServices() {
   async function waitForWebServer() {
     await retry(fetchStatusPage, {
       retries: 100,
+      maxTimeout: 1000,
+      minTimeout: 100,
     });
 
-    async function fetchStatusPage() {
+    async function fetchStatusPage(bail, tryNumber) {
+      console.log(`Attempt ${tryNumber} to fetch status page`);
       const response = await fetch(`http://localhost:3000/api/v1/status`);
       if (response.status !== 200) {
         throw new Error('Status page not ready');
